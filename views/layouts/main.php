ed4c5515ace6ab88a8fd97ea89b8e7edb9ebef73
Andrey Klochok <andreyklochok@gmail.com>
2016-04-07 12:46:27 +0300

    minor

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -10,7 +10,6 @@ use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($this);
-$this->registerJs("$(':checkbox').radiocheck();");
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
